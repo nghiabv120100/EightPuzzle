42a0147bdc9380b08b461680b567578b0349c072
jstD <dung.truong2000@hcmut.edu.vn>
2021-07-01 23:34:36 +0700

    A*, but time-consuming

--- EightPuzzle.js
+++ EightPuzzle.js
@@ -1,9 +1,3 @@
-class Point{
-    constructor(x,y){
-        this.x = x;
-        this.y = y;
-    }
-}
 /*
 This is only work for 2d board
 */
@@ -19,91 +13,241 @@ function deepcopy(board){
     }
     return cop;
 }
+class Point{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+}
+class State{
+    constructor(board,cost,point,parent,step){
+        this.board = board;
+        this.cost = cost
+        this.point = point;
+        this.parent = parent;
+        this.prev_step = step;
+    }
+}
+
+/*
+    Priority queue, https://lowrey.me/priority-queue-in-es6-javascript/
+*/
+class PriorityQueue {
+    constructor() {
+        this.data = [];
+    }
+    push(value, priority = 0) {
+        return this.data.push({
+            value: value,
+            priority: priority
+        });
+    }
+    pop() {
+        let index = 0;
+        let min = Infinity;
+        for (let i = 0; i < this.data.length; i++) {
+            let priority = this.data[i].priority;
+            if (Math.min(min, priority) === priority) {
+            min = priority;
+            index = i;
+            }
+        }
+        return this.data.splice(index, 1)[0].value;
+    }
+    size() {
+        return this.data.length;
+    }
+}
+/*
+    Main class, implement some search algorithms on 8-Puzzle problems
+*/
 class EightPuzzle{
-    constructor(){
-        this.state,this.point = generateInit();
-        this.goal = [[0,1,2],
-                    [3,4,5],
-                    [6,7,8]];
+    constructor(state){
+        this.state = state;
     }
-    generateInit(){
-        return [[ 2, 3, 4],
-                [ 1, 5, 7],
-                [ 8, 0, 6]], Point(2,1);
-    }
-    left(board,point){
+    left(state){
+        let board = deepcopy(state.board);
+        let point = state.point;
         let x = point.x;
         let y = point.y;
         if(y>0){
             board[x][y] = board[x][y-1];
             board[x][y-1] = 0;
-            return board,Point(x,y-1)
+            return new State(board,state.cost+1,new Point(x,y-1),state,'L');
         }
         else{
-            return None;
+            return null;
         }
     }
-    right(board,point){
+    right(state){
+        let board = deepcopy(state.board);
+        let point = state.point;
         let x = point.x;
         let y = point.y;
         if(y<2){
             board[x][y] = board[x][y+1];
             board[x][y+1] = 0;
-            return board,Point(x,y+1)
+            return new State(board,state.cost+1,new Point(x,y+1),state,'R');
         }
         else{
-            return None;
+            return null;
         }
     }
-    up(board,point){
+    up(state){
+        let board = deepcopy(state.board);
+        let point = state.point;
         let x = point.x;
         let y = point.y;
         if(x>0){
             board[x][y] = board[x-1][y];
             board[x-1][y] = 0;
-            return board,Point(x-1,y)
+            return new State(board,state.cost+1,new Point(x-1,y),state,'U');
         }
         else{
-            return None;
+            return null;
         }
     }
-    down(board,point){
+    down(state){
+        let board = deepcopy(state.board);
+        let point = state.point;
         let x = point.x;
         let y = point.y;
         if(x<2){
             board[x][y] = board[x+1][y];
             board[x+1][y] = 0;
-            return board,Point(x,y+1)
+            return new State(board,state.cost+1,new Point(x+1,y),state,'D');
         }
         else{
-            return None;
+            return null;
         }
     }
-    gen_all_steps(board,point){
+    
+    gen_all_steps(){
+        /*
+            Output: Array of all valid next states 
+        */
         let lst = [];
-        let current = deepcopy(board);
-        left = this.left(current,point);
+        let left = this.left(this.state);
         if(left){
             lst.push(left);
         }
 
-        current = deepcopy(board);
-        right = this.right(current,point);
+        let right = this.right(this.state);
         if(right){
             lst.push(right);
         }
 
-        current = deepcopy(board);
-        top = this.top(current,point);
-        if(top){
-            lst.push(top);
+        let up = this.up(this.state);
+        if(up){
+            lst.push(up);
         }
 
-        current = deepcopy(board);
-        down = this.down(current,point);
+        let down = this.down(this.state);
         if(down){
             lst.push(down);
         }
         return lst;
     }
+    is_goal(board){
+        oy = board.length 
+        ox = board[0].length
+        for(var i =0;i<oy;i++){
+            for(var j =0;j<ox;j++){
+                if(board[i][j]!=this.goal[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
 }
+/*
+    This function is used to scored the board with Manhattan distance
+    */
+let goal =  [[0,1,2],
+            [3,4,5],
+            [6,7,8]];
+function score(board){
+    let score = 0;
+    oy = board.length 
+    ox = board[0].length
+    for(var i =0;i<oy;i++){
+        for(var j =0;j<ox;j++){
+            value = board[i][j];
+            if(value>0){
+                let x = value%ox;
+                let y = Math.floor(value/oy);
+                score += Math.abs(x-j)+Math.abs(y-i);
+            }
+        }
+    }
+    return score;
+}
+// function score(board){
+//     let score = 0;
+//     oy = board.length;
+//     ox = board[0].length;
+//     for(var i =0;i<oy;i++){
+//         for(var j =0;j<ox;j++){
+//             if(board[i][j]!=i*oy+j && board[i][j]!=0){
+//                 score +=1;
+//             }
+//         }
+//     }
+//     return score;
+// }
+
+
+function generateInit(){
+    let board = [[ 1,0,2],
+                [ 6,4,8],
+                [ 5,3,7]];
+    return new State(board, 0, new Point(0,1),null,null);
+}
+
+function a_star_heuristic(state){
+    return state.cost*2 + score(state.board); f = h + g
+}
+
+function gen_path(state){
+    path = [state]
+    while(state.parent){
+        state = state.parent;
+        path.unshift(state);
+    }
+    return path;
+}
+
+function a_star_search(init_state){
+    queue = new PriorityQueue();
+    let path = []
+    queue.push(init_state,a_star_heuristic(init_state));
+    while(true){
+        state = queue.pop();
+        if(score(state.board)==0){
+            path = gen_path(state);
+            break;
+        }
+        else{
+            puzzle = new EightPuzzle(state);
+            lst_next = puzzle.gen_all_steps();
+            lst_next.forEach((element)=>{
+                queue.push(element,a_star_heuristic(element));
+            });
+        }
+    }
+    path.forEach((element)=>{
+        console.log(element);
+    })
+}
+let init = generateInit();
+a_star_search(init);
+// let queue = new PriorityQueue();
+// queue.push(init);
+
+// init = queue.pop();
+// puzzle = new EightPuzzle(init);
+// lst_next = puzzle.gen_all_steps();
+// lst_next.forEach((element)=>{
+//     queue.push(element,a_star_heuristic(element));
+// });
+// console.log(queue);
--- main.js
+++ main.js
@@ -1,77 +1,77 @@
-import {EightPuzzle} from EightPuzzle;
-var initial = [ [-1,-1,-1,-1,-1],
-                [-1, 2, 3, 4,-1],
-                [-1, 1, 5, 7,-1],
-                [-1, 8, 0, 6,-1],
-                [-1,-1,-1,-1,-1]]
+// import {EightPuzzle} from EightPuzzle;
+// var initial = [ [-1,-1,-1,-1,-1],
+//                 [-1, 2, 3, 4,-1],
+//                 [-1, 1, 5, 7,-1],
+//                 [-1, 8, 0, 6,-1],
+//                 [-1,-1,-1,-1,-1]]
 
 
-var goal =    [ [-1,-1,-1,-1,-1],
-                [-1, 0, 1, 2,-1],
-                [-1, 3, 4, 5,-1],
-                [-1, 6, 7, 8,-1],
-                [-1,-1,-1,-1,-1]]
+// var goal =    [ [-1,-1,-1,-1,-1],
+//                 [-1, 0, 1, 2,-1],
+//                 [-1, 3, 4, 5,-1],
+//                 [-1, 6, 7, 8,-1],
+//                 [-1,-1,-1,-1,-1]]
 
-window.onload = function() {
-    // var puzzle = new EightPuzzle() ;
-    drawPuzzle(initial);
-}
+// window.onload = function() {
+//     // var puzzle = new EightPuzzle() ;
+//     drawPuzzle(initial);
+// }
 
-drawPuzzle = function() {
+// drawPuzzle = function() {
    
-    var view = document.getElementById('view-puzzle');
-    view.innerHTML="";
-    for (var i=1;i<4;i++) {
-        var row = document.createElement("div");
-        for (var j=1; j<4; j++) {
-            let node = document.createElement("input");
-            node.type = "button";
-            node.value = initial[i][j];
-            node.addEventListener('click',function() {          
-                swapNode(node.value)
-            });
-            row.appendChild(node);
-        }
-        view.appendChild(row);
-    }
-}
+//     var view = document.getElementById('view-puzzle');
+//     view.innerHTML="";
+//     for (var i=1;i<4;i++) {
+//         var row = document.createElement("div");
+//         for (var j=1; j<4; j++) {
+//             let node = document.createElement("input");
+//             node.type = "button";
+//             node.value = initial[i][j];
+//             node.addEventListener('click',function() {          
+//                 swapNode(node.value)
+//             });
+//             row.appendChild(node);
+//         }
+//         view.appendChild(row);
+//     }
+// }
 
-function swapNode(value) {
+// function swapNode(value) {
 
-    var xA,yA; // a position of '0'
-    var xB,yB; // a position of node clicked
-    console.log(value)
-    for (let i = 1; i < 4; i++) {
-        for (let j = 1; j < 4; j++) {
-            console.log(initial[i])
-            //Get a position of "0"
-            if (initial[i][j] == 0) {
-                xA=i;
-                yA=j;
-            }
-            //Get a position of node clicked
-            if (initial[i][j] == value) {
-                xB=i;
-                yB=j;
-            }
-        }
-    }
+//     var xA,yA; // a position of '0'
+//     var xB,yB; // a position of node clicked
+//     console.log(value)
+//     for (let i = 1; i < 4; i++) {
+//         for (let j = 1; j < 4; j++) {
+//             console.log(initial[i])
+//             //Get a position of "0"
+//             if (initial[i][j] == 0) {
+//                 xA=i;
+//                 yA=j;
+//             }
+//             //Get a position of node clicked
+//             if (initial[i][j] == value) {
+//                 xB=i;
+//                 yB=j;
+//             }
+//         }
+//     }
 
-    // Swap
-    if (initial[xB-1][yB] == 0 || initial[xB+1][yB] == 0 || initial[xB][yB-1] == 0 || initial[xB][yB+1] == 0) {
-        // console.log(initial)
-        initial[xA][yA] = initial[xB][yB];
-        initial[xB][yB] = 0;
-        // console.log(puzzle)
-    }
-    drawPuzzle(initial);
+//     // Swap
+//     if (initial[xB-1][yB] == 0 || initial[xB+1][yB] == 0 || initial[xB][yB-1] == 0 || initial[xB][yB+1] == 0) {
+//         // console.log(initial)
+//         initial[xA][yA] = initial[xB][yB];
+//         initial[xB][yB] = 0;
+//         // console.log(puzzle)
+//     }
+//     drawPuzzle(initial);
 
-    let flag = isWin();
-    if (flag == true) {
-        console.log('You win!')
-    }
-}
+//     let flag = isWin();
+//     if (flag == true) {
+//         console.log('You win!')
+//     }
+// }
 
-function isWin() {
-    return JSON.stringify(initial) === JSON.stringify(goal)
-}
+// function isWin() {
+//     return JSON.stringify(initial) === JSON.stringify(goal)
+// }
